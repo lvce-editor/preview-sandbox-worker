71f03dda16324e16420259a0f582f70296201f38
Le Vivilet <levivilet@pm.me>
2026-02-10 16:13:17 +0100

    refactor: update CreateWindowResult interface to use specific types for document and window

--- packages/preview-sandbox-worker/src/parts/CreateWindow/CreateWindow.ts
+++ packages/preview-sandbox-worker/src/parts/CreateWindow/CreateWindow.ts
@@ -1,9 +1,12 @@
+import type { Document } from 'happy-dom-without-node';
 import { Window } from 'happy-dom-without-node'
 
 export interface CreateWindowResult {
-  readonly document: any
-  readonly window: any
+  readonly document: Document
+  readonly window: Window
 }
+
+
 
 export const createWindow = (rawHtml: string): CreateWindowResult => {
   const window = new Window({ url: 'https://localhost:3000' })
