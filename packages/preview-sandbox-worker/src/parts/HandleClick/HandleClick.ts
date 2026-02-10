c1cd5a114a9e0b2095633484a9912733944a1d30
Le Vivilet <levivilet@pm.me>
2026-02-10 18:07:54 +0100

    refactor: enhance dispatchClickEvent and handleClick to include clientX and clientY parameters for improved event handling (#19)

--- packages/preview-sandbox-worker/src/parts/HandleClick/HandleClick.ts
+++ packages/preview-sandbox-worker/src/parts/HandleClick/HandleClick.ts
@@ -2,7 +2,7 @@ import type { PreviewState } from '../PreviewState/PreviewState.ts'
 import * as DispatchClickEvent from '../DispatchClickEvent/DispatchClickEvent.ts'
 import * as HappyDomState from '../HappyDomState/HappyDomState.ts'
 
-const handleClickLocal = (uid: number, hdId: string): any => {
+const handleClickLocal = (uid: number, hdId: string, clientX: number = 0, clientY: number = 0, x: number = 0, y: number = 0): any => {
   const happyDomInstance = HappyDomState.get(uid)
   if (!happyDomInstance) {
     return
@@ -12,7 +12,9 @@ const handleClickLocal = (uid: number, hdId: string): any => {
     return
   }
 
-  DispatchClickEvent.dispatchClickEvent(element, happyDomInstance.window)
+  const adjustedClientX = clientX - x
+  const adjustedClientY = clientY - y
+  DispatchClickEvent.dispatchClickEvent(element, happyDomInstance.window, adjustedClientX, adjustedClientY)
 
   const elementMap = new Map<string, any>()
 
@@ -23,6 +25,13 @@ const handleClickLocal = (uid: number, hdId: string): any => {
   })
 }
 
-export const handleClick = (uid: number, hdId: string): PreviewState | Promise<PreviewState> => {
-  return handleClickLocal(uid, hdId)
+export const handleClick = (
+  uid: number,
+  hdId: string,
+  clientX: number = 0,
+  clientY: number = 0,
+  x: number = 0,
+  y: number = 0,
+): PreviewState | Promise<PreviewState> => {
+  return handleClickLocal(uid, hdId, clientX, clientY, x, y)
 }
