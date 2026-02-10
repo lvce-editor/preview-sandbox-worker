6b28ce5bf460d318082eb92469d31a30c5671a6e
Le Vivilet <levivilet@pm.me>
2026-02-10 18:07:01 +0100

    refactor: remove unused Renderer and SaveState interfaces for cleaner codebase (#18)

--- packages/preview-sandbox-worker/src/parts/SaveState/SaveState.ts
+++
@@ -1,8 +0,0 @@
-import type { PreviewState } from '../PreviewState/PreviewState.ts'
-import type { SavedState } from '../SavedState/SavedState.ts'
-
-export const saveState = (state: PreviewState): SavedState => {
-  return {
-    x: 0,
-  }
-}
