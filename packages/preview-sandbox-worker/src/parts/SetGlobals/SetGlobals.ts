fb2513846aa2cb43858aa0d6b80f7f42098e4273
Le Vivilet <levivilet@pm.me>
2026-02-10 18:49:28 +0100

    fix

--- packages/preview-sandbox-worker/src/parts/SetGlobals/SetGlobals.ts
+++ packages/preview-sandbox-worker/src/parts/SetGlobals/SetGlobals.ts
@@ -3,18 +3,12 @@ import type { Window } from 'happy-dom-without-node'
 import type { Globals } from '../GetGlobals/GetGlobals.ts'
 
 export const setGlobals = (window: Window, globalGlobals: Globals['globalGlobals'], windowGlobals: Globals['windowGlobals']): void => {
-  // @ts-ignore
-  window.alert = windowGlobals.alert
-  // @ts-ignore
-  globalThis.alert = globalGlobals.alert
-  // @ts-ignore
-  window.localStorage = windowGlobals.localStorage
-  // @ts-ignore
-  globalThis.localStorage = globalGlobals.localStorage
-  window.innerWidth = windowGlobals.innerWidth
-  window.innerHeight = windowGlobals.innerHeight
-  // @ts-ignore
-  globalThis.innerWidth = globalGlobals.innerWidth
-  // @ts-ignore
-  globalThis.innerHeight = globalGlobals.innerHeight
+  for (const [key, value] of Object.entries(globalGlobals)) {
+    // @ts-ignore
+    globalThis[key] = value
+  }
+  for (const [key, value] of Object.entries(windowGlobals)) {
+    // @ts-ignore
+    window[key] = value
+  }
 }
