bf35ff9a000fd7821cc5d0ca44e05a655abb320e
Le Vivilet <levivilet@pm.me>
2026-02-10 18:10:08 +0100

    refactor: simplify handleClick and handleMousedown functions by removing unused parameters for cleaner code

--- packages/preview-sandbox-worker/src/parts/HandleClick/HandleClick.ts
+++ packages/preview-sandbox-worker/src/parts/HandleClick/HandleClick.ts
@@ -2,7 +2,7 @@ import type { PreviewState } from '../PreviewState/PreviewState.ts'
 import * as DispatchClickEvent from '../DispatchClickEvent/DispatchClickEvent.ts'
 import * as HappyDomState from '../HappyDomState/HappyDomState.ts'
 
-const handleClickLocal = (uid: number, hdId: string, clientX: number = 0, clientY: number = 0, x: number = 0, y: number = 0): any => {
+const handleClickLocal = (uid: number, hdId: string, clientX: number, clientY: number): any => {
   const happyDomInstance = HappyDomState.get(uid)
   if (!happyDomInstance) {
     return
@@ -12,9 +12,7 @@ const handleClickLocal = (uid: number, hdId: string, clientX: number = 0, client
     return
   }
 
-  const adjustedClientX = clientX - x
-  const adjustedClientY = clientY - y
-  DispatchClickEvent.dispatchClickEvent(element, happyDomInstance.window, adjustedClientX, adjustedClientY)
+  DispatchClickEvent.dispatchClickEvent(element, happyDomInstance.window, clientX, clientY)
 
   const elementMap = new Map<string, any>()
 
@@ -25,13 +23,6 @@ const handleClickLocal = (uid: number, hdId: string, clientX: number = 0, client
   })
 }
 
-export const handleClick = (
-  uid: number,
-  hdId: string,
-  clientX: number = 0,
-  clientY: number = 0,
-  x: number = 0,
-  y: number = 0,
-): PreviewState | Promise<PreviewState> => {
-  return handleClickLocal(uid, hdId, clientX, clientY, x, y)
+export const handleClick = (uid: number, hdId: string, clientX: number = 0, clientY: number = 0): PreviewState | Promise<PreviewState> => {
+  return handleClickLocal(uid, hdId, clientX, clientY)
 }
