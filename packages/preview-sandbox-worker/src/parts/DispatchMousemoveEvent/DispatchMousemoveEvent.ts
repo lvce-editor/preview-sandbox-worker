08e74b7723ce8c878c4764393b37d26caf4343b9
Le Vivilet <levivilet@pm.me>
2026-02-25 10:50:50 +0100

    feature: update dispatchMousemoveEvent to use Window type for window parameter (#33)

--- packages/preview-sandbox-worker/src/parts/DispatchMousemoveEvent/DispatchMousemoveEvent.ts
+++ packages/preview-sandbox-worker/src/parts/DispatchMousemoveEvent/DispatchMousemoveEvent.ts
@@ -1,6 +1,8 @@
+/* eslint-disable @typescript-eslint/prefer-readonly-parameter-types */
+import type { Window } from 'happy-dom-without-node'
 import * as DispatchEvent from '../DispatchEvent/DispatchEvent.ts'
 
-export const dispatchMousemoveEvent = (element: any, window: any, clientX: number = 0, clientY: number = 0): void => {
+export const dispatchMousemoveEvent = (element: any, window: Window, clientX: number = 0, clientY: number = 0): void => {
   const mousemoveEvent = new window.MouseEvent('mousemove', { bubbles: true, clientX, clientY })
   DispatchEvent.dispatchEvent(element, mousemoveEvent)
 }
