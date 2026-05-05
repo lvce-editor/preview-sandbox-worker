e5ef6b4776158605b5f969a19d19d84e95b0ed1d
Le Vivilet <levivilet@pm.me>
2026-05-05 17:33:20 +0200

    skip

--- packages/e2e/src/preview-keydown-event.ts
+++ packages/e2e/src/preview-keydown-event.ts
@@ -2,6 +2,8 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'preview.keydown-event'
 
+export const skip = 1
+
 export const test: Test = async ({ Command, expect, FileSystem, Locator, Preview, Workspace }) => {
   // arrange
   const tmpDir = await FileSystem.getTmpDir()
