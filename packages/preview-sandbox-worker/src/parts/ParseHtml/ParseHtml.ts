0750eb6a8fa7c0b04bbe5de313f9d7a6840b8e25
Le Vivilet <levivilet@pm.me>
2026-07-19 17:08:50 +0200

    feature: enable recommended virtual dom lint rules (#70)

--- packages/preview-sandbox-worker/src/parts/ParseHtml/ParseHtml.ts
+++ packages/preview-sandbox-worker/src/parts/ParseHtml/ParseHtml.ts
@@ -1,5 +1,5 @@
 import type { VirtualDomNode } from '@lvce-editor/virtual-dom-worker'
-import { text } from '@lvce-editor/virtual-dom-worker'
+import { text, VirtualDomElements } from '@lvce-editor/virtual-dom-worker'
 import * as Assert from '../Assert/Assert.ts'
 import * as GetVirtualDomTag from '../GetVirtualDomTag/GetVirtualDomTag.ts'
 import * as HtmlTokenType from '../HtmlTokenType/HtmlTokenType.ts'
@@ -40,7 +40,7 @@ export const parseHtml = (html: string, allowedAttributes: readonly string[] = [
   const scripts: string[] = []
   const root: VirtualDomNode = {
     childCount: 0,
-    type: 0,
+    type: VirtualDomElements.Root,
   }
   let current: any = root
   const stack: VirtualDomNode[] = [root]
