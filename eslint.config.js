465a58c5372de7a73dfd817232f239154b9e4111
Le Vivilet <levivilet@pm.me>
2026-08-01 13:30:08 -0700

    feature: update eslint dependencies (#76)

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,10 @@
+import { defineConfig } from 'eslint/config'
 import * as config from '@lvce-editor/eslint-config'
-import * as actions from '@lvce-editor/eslint-plugin-github-actions'
 
-export default [
+export default defineConfig([
   ...config.default,
   ...config.recommendedVirtualDom,
-  ...actions.default,
+  ...config.recommendedActions,
   {
     files: ['**/*.ts'],
     rules: {
@@ -77,4 +77,4 @@ export default [
       'virtual-dom/valid-child-count': 'off',
     },
   },
-]
+])
